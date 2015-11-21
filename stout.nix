afed83ac20ff6c0647d16ce7c61266f2e6b32f86
Ryan Seto <mr.werewolf@gmail.com>
2015-11-21 17:45:28 -0500

    Add new machine: stout

---
+++ stout.nix
@@ -0,0 +1,68 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+    ./common.nix
+  ];
+
+  networking = {
+    hostId = "a794f064";
+    hostName = "stout";
+  };
+
+  boot = {
+    kernelModules = [
+      "kvm-intel"
+    ];
+
+    extraModulePackages = [ ];
+    kernelPackages = pkgs.linuxPackages_4_2;
+
+    initrd = {
+      availableKernelModules = [
+        "uhci_hcd"
+        "ehci_pci"
+        "ata_piix"
+        "usb_storage"
+        "floppy"
+      ];
+
+      luks = {
+        devices = [{
+          name = "nixos-root";
+          device = "/dev/sda2";
+          allowDiscards = true;
+        }];
+      };
+    };
+
+    zfs = {
+      forceImportRoot = false;
+      forceImportAll = false;
+    };
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "rpool/ROOT/nixos";
+      fsType = "zfs";
+    };
+
+    "/home" = {
+      device = "rpool/home";
+      fsType = "zfs";
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-label/boot";
+      fsType = "ext2";
+    };
+  };
+
+  swapDevices = [
+    { device = "/dev/zvol/rpool/swap"; }
+  ];
+
+  nix.maxJobs = 4;
+}
